1c480708904d06070030c1505ee9d687b2b7f008
guosj <515878133@qq.com>
2022-05-02 10:53:41 +0800

    add quick sort implementation

---
+++ quick-sort/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{9, 7, 3, 6, 8, 7}
+	quickSort(a)
+	fmt.Println(a)
+}
+
+func quickSort(list []int) {
+	if len(list) < 2 {
+		return
+	}
+	i, j := 0, len(list)-1
+	pivot := list[i]
+	for i < j {
+		for i < j && list[j] > pivot {
+			j--
+		}
+		if i < j {
+			list[i] = list[j]
+			i++
+		}
+		for i < j && list[i] <= pivot {
+			i++
+		}
+		if i < j {
+			list[j] = list[i]
+			j--
+		}
+	}
+	list[i] = pivot
+	quickSort(list[:i])
+	quickSort(list[i+1:])
+}
