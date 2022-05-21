c343bb4ca39baa8b6480c42013f68d5e51d770ec
guosj <515878133@qq.com>
2022-05-21 10:01:36 +0800

    [bfs] update code

--- bfs/main.go
+++ bfs/main.go
@@ -1,67 +1,89 @@
-// Example code for https://guo-sj.github.io/algorithm/2022/04/19/bfs.html
 package main
 
 import (
 	"fmt"
-	"log"
 )
 
-type netGraph map[string][]string
+var (
+	netGraph          map[string][]string
+	personQueen       []string
+	personStack       []string
+	bfsSearchedPerson map[string]bool
+	dfsSearchedPerson map[string]bool
+)
 
-type personQueen []string
+func init() {
+	netGraph = make(map[string][]string)
+	netGraph["you"] = []string{"alice", "bob", "claire"}
+	netGraph["bob"] = []string{"anuj", "peggy"}
+	netGraph["alice"] = []string{"peggy"}
+	netGraph["claire"] = []string{"thom", "jonny"}
+	netGraph["anuj"] = []string{}
+	netGraph["peggy"] = []string{}
+	netGraph["thom"] = []string{}
+	netGraph["jonny"] = []string{}
 
-type searchedPerson map[string]bool
-
-func (ng *netGraph) initGraph() {
-	*ng = make(netGraph)
-	(*ng)["you"] = []string{"alice", "bob", "claire"}
-	(*ng)["bob"] = []string{"anuj", "peggy"}
-	(*ng)["alice"] = []string{"peggy"}
-	(*ng)["claire"] = []string{"thom", "jonny"}
-	(*ng)["anuj"] = []string{""}
-	(*ng)["peggy"] = []string{""}
-	(*ng)["thom"] = []string{""}
-	(*ng)["jonny"] = []string{""}
+	bfsSearchedPerson = make(map[string]bool)
+	dfsSearchedPerson = make(map[string]bool)
 }
 
-func (pq *personQueen) add(people []string) {
-	log.Printf("add %v to pq\n", people)
+// addPerson adds people to a stack or queue
+func addPerson(pq *[]string, people []string) {
 	for i := range people {
 		*pq = append(*pq, people[i])
 	}
 }
 
-func (sp *searchedPerson) add(person string) {
-	(*sp)[person] = true
-}
-
 func is_mango_seller(person string) bool {
-	log.Printf("is_mango_seller: %s\n", person)
 	return person[len(person)-1] == 'm'
 }
 
 func bfs() bool {
-	var pq personQueen
-	var ng netGraph
-	ng.initGraph()
-	pq.add(ng["you"])
+	addPerson(&personQueen, netGraph["you"])
+	fmt.Printf("you ")
 
-	sq := make(searchedPerson)
-	for i := 0; i < len(pq); i++ {
-		p := pq[i]
-		if sq[p] {
+	for len(personQueen) > 0 {
+		p := personQueen[0]
+		personQueen = personQueen[1:]
+		if bfsSearchedPerson[p] {
 			continue
 		}
+		fmt.Printf("-> %s", p)
 		if is_mango_seller(p) {
+			fmt.Printf("\n")
 			fmt.Printf("%s is a mango seller\n", p)
 			return true
 		}
-		sq.add(p)
-		pq.add(ng[p])
+		bfsSearchedPerson[p] = true
+		addPerson(&personQueen, netGraph[p])
+	}
+	return false
+}
+
+// add dfs implementation for practice
+func dfs() bool {
+	addPerson(&personStack, netGraph["you"])
+	fmt.Printf("you ")
+
+	for len(personStack) > 0 {
+		p := personStack[len(personStack)-1]
+		personStack = personStack[:len(personStack)-1]
+		if dfsSearchedPerson[p] {
+			continue
+		}
+		fmt.Printf("-> %s", p)
+		if is_mango_seller(p) {
+			fmt.Printf("\n")
+			fmt.Printf("%s is a mango seller\n", p)
+			return true
+		}
+		dfsSearchedPerson[p] = true
+		addPerson(&personStack, netGraph[p])
 	}
 	return false
 }
 
 func main() {
 	fmt.Println(bfs())
+	fmt.Println(dfs())
 }
